c9f5591211510c3a2c2f255ed6169ffc043682f0
bessekratte <34283405+bessekratte@users.noreply.github.com>
2019-03-02 16:31:57 +0100

    add .gitignore file

--- src/main/java/pl/kierznowski/tika/TikaService.java
+++ src/main/java/pl/kierznowski/tika/TikaService.java
@@ -1,12 +1,8 @@
 package pl.kierznowski.tika;
 
-import org.apache.tika.exception.TikaException;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 public interface TikaService {
 
     String parseFileIntoPlainText(MultipartFile file) ;
-
 }
